de77da5ad384e2a6581f6e697c6e77185a28fca6
Petro Sernivka <40458975+petrosernivka@users.noreply.github.com>
2018-08-09 14:46:23 +0300

    Update Eight_ways_to_iterate_over_table.py

--- Eight_ways_to_iterate_over_table.py
+++ Eight_ways_to_iterate_over_table.py
@@ -4,66 +4,51 @@ class Table:
     def __init__(self,data):
         self.data = data
     def walk(self,dir0,dir1):
-        k = 3000
-        if len(self.data[0]) < k:
-            res = []
-            if dir0 == 4 and dir1 == 3: #вправо-вниз
-                list(map(lambda i: res.extend(i), self.data))
-            elif dir0 == 3 and dir1 == 4: #вниз-вправо
-                for j in range(len(self.data[0])):
-                    for i in self.data:
-                        res.append(i[j])
-            elif dir0 == 2 and dir1 == 3: #вліво-вниз
-                list(map(lambda i: res.extend(i[::-1]), self.data))
-            elif dir0 == 3 and dir1 == 2: #вниз-вліво
-                for j in range(len(self.data[0])):
-                    for i in self.data:
-                        res.append(i[::-1][j])
-            elif dir0 == 4 and dir1 == 1: #вправо-вверх
-                list(map(lambda i: res.extend(i), self.data[::-1]))
-            elif dir0 == 1 and dir1 == 4: #вверх-вправо
-                for j in range(len(self.data[0])):
-                    for i in self.data[::-1]:
-                        res.append(i[j])
-            elif dir0 == 2 and dir1 == 1: #вліво-вверх
-                list(map(lambda i: res.extend(i[::-1]), self.data[::-1]))
-            elif dir0 == 1 and dir1 == 2: #вверх-вліво
-                for j in range(len(self.data[0])):
-                    for i in self.data[::-1]:
-                        res.append(i[::-1][j])
-            return res
-        else:
-            res = []
-            if dir0 == 4 and dir1 == 3: #вправо-вниз
-                res = list(self.data[0][:k])
-            elif dir0 == 3 and dir1 == 4: #вниз-вправо
-                for j in range(k//4):
-                    res.append(list(self.data[0][:k//4])[j])
-                    res.append(list(self.data[1][:k//4])[j])
-                    res.append(list(self.data[2][:k//4])[j])
-                    res.append(list(self.data[3][:k//4])[j])
-            elif dir0 == 2 and dir1 == 3: #вліво-вниз
-                res = list(self.data[0][-k:])[::-1]
-            elif dir0 == 3 and dir1 == 2: #вниз-вліво
-                for j in range(k//4):
-                    res.append(list(self.data[0][-k//4:])[::-1][j])
-                    res.append(list(self.data[1][-k//4:])[::-1][j])
-                    res.append(list(self.data[2][-k//4:])[::-1][j])
-                    res.append(list(self.data[3][-k//4:])[::-1][j])
-            elif dir0 == 4 and dir1 == 1: #вправо-вверх
-                res = list(self.data[-1][:k])
-            elif dir0 == 1 and dir1 == 4: #вверх-вправо
-                for j in range(k//4):
-                    res.append(list(self.data[3][:k//4])[j])
-                    res.append(list(self.data[2][:k//4])[j])
-                    res.append(list(self.data[1][:k//4])[j])
-                    res.append(list(self.data[0][:k//4])[j])
-            elif dir0 == 2 and dir1 == 1: #вліво-вверх
-                res = list(self.data[-1][-k:])[::-1]
-            elif dir0 == 1 and dir1 == 2: #вверх-вліво
-                for j in range(k//4):
-                    res.append(list(self.data[3][-k//4:])[::-1][j])
-                    res.append(list(self.data[2][-k//4:])[::-1][j])
-                    res.append(list(self.data[1][-k//4:])[::-1][j])
-                    res.append(list(self.data[0][-k//4:])[::-1][j])
-            return res
+        
+        #вправо-вниз
+        if dir0 == 4 and dir1 == 3: #вправо-вниз
+            for j in range(len(self.data)):
+                for i in range(len(self.data[0])):
+                    yield self.data[j][i]
+        
+        #вниз-вправо
+        elif dir0 == 3 and dir1 == 4: 
+            for j in range(len(self.data[0])):
+                for i in range(len(self.data)):
+                    yield self.data[i][j]
+        
+        #вліво-вниз
+        elif dir0 == 2 and dir1 == 3: 
+            for j in range(len(self.data)):
+                for i in range(len(self.data[0])-1, -1, -1):
+                    yield self.data[j][i]
+            
+        #вниз-вліво
+        elif dir0 == 3 and dir1 == 2: 
+            for j in range(len(self.data[0])-1, -1, -1):
+                for i in range(len(self.data)):
+                    yield self.data[i][j]
+                    
+        #вправо-вверх
+        elif dir0 == 4 and dir1 == 1: 
+            for j in range(len(self.data)-1, -1, -1):
+                for i in range(len(self.data[0])):
+                    yield self.data[j][i]
+            
+        #вверх-вправо    
+        elif dir0 == 1 and dir1 == 4: 
+            for j in range(len(self.data[0])):
+                for i in range(len(self.data)-1, -1, -1):
+                    yield self.data[i][j]
+                    
+        #вліво-вверх            
+        elif dir0 == 2 and dir1 == 1: 
+            for j in range(len(self.data)-1, -1, -1):
+                for i in range(len(self.data[0])-1, -1, -1):
+                    yield self.data[j][i]
+            
+        #вверх-вліво    
+        elif dir0 == 1 and dir1 == 2: 
+            for j in range(len(self.data[0])-1, -1, -1):
+                for i in range(len(self.data)-1, -1, -1):
+                    yield self.data[i][j]
