7831b85a8fd17c5102b175e43094a22088083bc8
Petro Sernivka <40458975+petrosernivka@users.noreply.github.com>
2018-08-12 08:14:14 +0300

    Update Sum_of_Pairs_-_Timed_Out.py

--- Sum_of_Pairs_-_Timed_Out.py
+++ Sum_of_Pairs_-_Timed_Out.py
@@ -1,30 +1,30 @@
-1. Execution Timed Out:
+#1. Execution Timed Out:
 
 def sum_pairs(ints, s):
     for n in range(1, len(ints)):
         for m in range(0, n):
             if ints[n] + ints[m] == s:
                 return [ints[m], ints[n]]
---------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
                 
-2. Execution Timed Out:
+#2. Execution Timed Out:
 
 def sum_pairs(ints, s):
     for i in range(1, len(ints)):
         second_num = list(filter(lambda j: j + ints[i] == s , ints[0:i]))
         if second_num:
             return [second_num[0], ints[i]]
---------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
 
-3. Execution Timed Out:
+#3. Execution Timed Out:
 
 def sum_pairs(ints, s):
     for i in range(1, len(ints)):
         if s - ints[i] in ints[0: i]:
             return [s - ints[i], ints[i]]
---------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
 
-4. Exit Code: 137
+#4. Exit Code: 137
 
 def sum_pairs(ints, s):
     import itertools
